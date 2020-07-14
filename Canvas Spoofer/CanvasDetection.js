21619690626defdf32731e6f82da156fba3da6bc
smophos <saumya.smo@gmail.com>
2020-07-14 12:33:51 -0500

    Create CanvasDetection.js
    Obtain image data through this file, use it as content script.

---
+++ Canvas Spoofer/CanvasDetection.js
@@ -0,0 +1,35 @@
+var inject = function() {
+
+    var spoofedcanvasimagedata = {};
+    
+    var overwrite = function(name) {
+        const oldcanvas = HTMLCanvasElement.prototype[name];
+        Object.defineProperty(HTMLCanvasElement.prototype, name, {
+            "value": function() {
+                //debugger;
+                //console.trace();
+                
+                var width = this.width,
+                    height = this.height,
+                    context = this.getContext("2d");
+            
+                var imageData = context.getImageData(0, 0, width, height);
+                console.log(imageData);
+                //context.putImageData(spoofedcanvasimagedata, 0, 0);
+                return oldcanvas.apply(this, arguments);
+            }
+        });
+    };
+
+
+    overwrite('toBlob');
+    overwrite('toDataURL');
+
+    document.documentElement.dataset.cbscriptallow = true;
+};
+
+// INJECTING INTO THE SCRIPT
+var script_1 = document.createElement('script');
+script_1.textContent = "(" + inject + ")()";
+document.documentElement.appendChild(script_1);
+
